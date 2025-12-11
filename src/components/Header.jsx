2fe6a00712b5eef0100fb50ca7e156c28ed3fbf2
reece <trazhme@gmail.com>
2025-12-11 08:40:39 +0800

    add mobile menu animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import Container from "./Container";
 import logo from "../assets/logo.svg";
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { useState } from "react";
 
 const Header = () => {
@@ -25,19 +25,21 @@ const Header = () => {
                     </div>
                 </div>
 
-                <div className="mobile-nav">
-                    <div className="flex items-center md:hidden justify-between">
+                <div className="mobile-nav md:hidden">
+                    <div className="flex items-center justify-between">
                         <div className="mobile-logo-container flex items-center">
                             <img className="h-20" src={logo} alt="logo" />
                             <h1 className="text-md uppercase font-bold text-slate-700">Ninja <span className="text-green-400">Dev</span></h1>
                         </div>
-
-                        <Menu onClick={() => setIsOpen((prev) => !prev) } className="h-8 w-8 text-slate-900 cursor-pointer" />
+                        {
+                            isOpen 
+                            ? <X onClick={() => setIsOpen(false) } className="h-8 w-8 text-slate-900 cursor-pointer" />
+                            : <Menu onClick={() => setIsOpen(true) } className="h-8 w-8 text-slate-900 cursor-pointer" />
+                        }
                     </div>
+
                     {
-                        isOpen &&
-                    <div className="mobile-nav-container relative bg-slate-400/40 px-5 py-5">
-                        <nav className="space-x-3 text-base uppercase font-semibold text-slate-700 tracking-wide">
+                    <div className={`mobile-nav-container overflow-hidden bg-slate-300/80 px-5 transition-all duration-300 ${isOpen ? 'max-h-96 opacity-100 py-5' : 'max-h-0 opacity-0 py-0'}`}>                        <nav className="space-x-3 text-base uppercase font-semibold text-slate-700 tracking-wide">
                             <a className="block py-2 my-2 border-b border-slate-400" href="#">Home</a>
                             <a className="block py-2 my-2 border-b border-slate-400" href="#">About</a>
                             <a className="block py-2 my-2 border-b border-slate-400" href="#">Products</a>
