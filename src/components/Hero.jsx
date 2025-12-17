512cacd79a4c0a3adfdf873febfc58d6ea8ce253
reece <trazhme@gmail.com>
2025-12-17 09:13:10 +0800

    adjust font size on desktop and mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,10 @@ const Hero = ({ hero, children }) => {
     return (
         <Section bg="bg-green-50 md:min-h-[calc(100dvh-100px)] flex items-center">
             <Container>
-                    <div className="flex justify-center items-center gap-10">
+                    <div className="flex md:flex-row flex-col justify-center items-center gap-10">
                         <div className="hero-content md:flex-1">
-                            <h1 className="text-5xl font-light mb-7 text-center md:text-left">Your Codebase's <span className="text-green-400">Secret Weapon.</span></h1>
-                            <h3 className="text-2xl/9 text-slate-600 font-light text-center md:text-left">With sharp instincts and lightning-fast problem-solving, this ninja developer transforms tricky challenges into clean, scalable, and reliable solutions.</h3>
+                            <h1 className="text-5xl/15 md:text-6xl/20  font-light mb-7 text-center md:text-left">Your Codebase's <span className="text-green-400">Secret Weapon.</span></h1>
+                            <h3 className="text-xl/9 md:text-2xl/9 text-slate-600 font-light text-center md:text-left">With sharp instincts and lightning-fast problem-solving, this ninja developer transforms tricky challenges into clean, scalable, and reliable solutions.</h3>
 
                             <div className="hero-buttons flex justify-center md:justify-self-start gap-5 mt-8">
                                 <button className="bg-slate-800 px-6 py-4 rounded text-white uppercase tracking-wide text-lg">Explore Codebase</button>
