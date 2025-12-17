428ca727469b1bb8e4362f7be91b892eece4d927
reece <trazhme@gmail.com>
2025-12-17 09:06:59 +0800

    add hero buttons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,6 +9,11 @@ const Hero = ({ hero, children }) => {
                         <div className="hero-content md:flex-1">
                             <h1 className="text-5xl font-light mb-7 text-center md:text-left">Your Codebase's <span className="text-green-400">Secret Weapon.</span></h1>
                             <h3 className="text-2xl/9 text-slate-600 font-light text-center md:text-left">With sharp instincts and lightning-fast problem-solving, this ninja developer transforms tricky challenges into clean, scalable, and reliable solutions.</h3>
+
+                            <div className="hero-buttons flex justify-center md:justify-self-start gap-5 mt-8">
+                                <button className="bg-slate-800 px-6 py-4 rounded text-white uppercase tracking-wide text-lg">Explore Codebase</button>
+                                <button className="bg-green-500 px-6 py-4 rounded text-white uppercase tracking-wider text-lg">Contact</button>
+                            </div>
                         </div>
 
                         <div className="hero-editor md:flex-1">
