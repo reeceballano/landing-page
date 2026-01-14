817f294142080bd9fd9a1d7235921bb9e12621d1
reece <trazhme@gmail.com>
2026-01-14 08:40:42 +0800

    remove dynamic class, add in style

--- src/components/Blurb/BlurbItem.jsx
+++ src/components/Blurb/BlurbItem.jsx
@@ -4,12 +4,17 @@ const BlurbItem = ({
     icon, background, size = 64, color = "currentColor",
     title, description
 }) => {
+
+    const style = {
+        backgroundColor: background,
+    }
+
     return (
         <div className="blurb border border-gray-100 flex-1 rounded-xl p-5 text-center flex flex-col items-center shadow-md">
-            <div className={`w-28 h-28 rounded-full bg-${background}-400 border border-gray-200 flex items-center justify-center`}>
+            <div style={style} className={`w-28 h-28 rounded-full border border-gray-200 flex items-center justify-center`}>
                 <CustomIcon name={icon} size={size} color={color}/>
             </div>
-            <h4 className="text-2xl my-4">{title}</h4>
+            <h4 className={`text-2xl my-4`}>{title}</h4>
             <p className="text-slate-500 font-light text-lg">{description}</p>
         </div>
     )
--- src/components/Blurb/Blurbs.jsx
+++ src/components/Blurb/Blurbs.jsx
@@ -2,10 +2,11 @@ import BlurbItem from "./BlurbItem";
 
 const Blurbs = ({ blurbs }) => {
     blurbs = [
-        { id: 1, icon: 'microscope', background: "red", title: "Research", description: "Personalize your offer and sell your product or offer directly in the funnel without further tools."},
-        { id: 2, icon: 'binary', background: "orange", title: "Develop", description: "Personalize your offer and sell your product or offer directly in the funnel without further tools."},
-        { id: 3, icon: 'terminal', background: "green", title: "Deploy", description: "Personalize your offer and sell your product or offer directly in the funnel without further tools."},
+        { id: 1, icon: 'microscope', background: "lightcoral", title: "Research", description: "Personalize your offer and sell your product or offer directly in the funnel without further tools."},
+        { id: 2, icon: 'binary', background: "lightsalmon", title: "Develop", description: "Personalize your offer and sell your product or offer directly in the funnel without further tools."},
+        { id: 3, icon: 'terminal', background: "lightgreen", title: "Deploy", description: "Personalize your offer and sell your product or offer directly in the funnel without further tools."},
     ]
+
     return (
         <div className="blurbs md:w-4/5 md:mx-auto flex flex-col md:flex-row gap-5 my-5 justify-around">
             {
