1b34f0084468bb7bf74809b2b934318cca4afba8
reece <trazhme@gmail.com>
2026-04-04 10:31:20 +0800

    update button mouse pointer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,7 +8,7 @@ const Button = ({
     return (
         <div className="button">
             <button
-                className={`${classes && classes} bg-slate-800 px-6 py-4 rounded text-white uppercase tracking-wide text-lg`}
+                className={`${classes && classes} bg-slate-800 px-6 py-4 rounded text-white uppercase tracking-wide text-lg cursor-pointer`}
                 type={type}
                 onClick={onClick}
             >
